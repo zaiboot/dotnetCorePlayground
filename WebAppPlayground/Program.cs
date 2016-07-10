81ca3f0fdaaa75b0bcb83706bb8916eef2419bea
zaiboot <zaiboot@gmail.com>
2016-07-10 13:47:09 -0600

    Adding krestel as guide recommendation

--- WebAppPlayground/Program.cs
+++ WebAppPlayground/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace WebAppPlayground
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
