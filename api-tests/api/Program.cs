ea65e08302312e4e9964134d3881a7ed6945e094
zaiboot <zaiboot@gmail.com>
2022-10-05 21:25:34 -0600

    Merging grafana dashboard

--- api-tests/api/Program.cs
+++ api-tests/api/Program.cs
@@ -7,13 +7,8 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 
 // Add services to the container.
 builder.Services.RegisterDependencies();
-builder.Services.AddControllers();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// removing for the moment
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
 builder.WebHost.UseUrls("http://localhost:5020");
 var app = builder.Build();
 app.MapRoutes();
